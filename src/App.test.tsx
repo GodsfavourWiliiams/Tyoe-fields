a28e35d87bc371d77ed07e17353d96c39abec105
Godsfavour Williams <williamsgodsfavoursolomon@gmail.com>
2023-04-19 02:56:46 -0700

    added test script for App.js
    added test script for App.js

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  describe('App component', () => {
+    test('renders type fields', () => {
+      render(<App />);
+      const header = screen.getByText(/Type Fields/i);
+      expect(header).toBeInTheDocument();
+    });
+  
+    test('renders a button to add a new field', () => {
+      render(<App />);
+      const addButton = screen.getByTestId('add field');
+      expect(addButton).toBeInTheDocument();
+    });
+  
+  });
