7f536d1353e6a33ee4e3c41de3adff5aa2309a7f
Godsfavour Williams <williamsgodsfavoursolomon@gmail.com>
2023-04-19 02:57:33 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -53,6 +53,7 @@ function App(): JSX.Element {
         <h1 className='font-bold text-lg'>Type Fields</h1>
         <button 
           onClick={handleAddField}
+          data-testid="add field"
           className="text-center p-2  rounded-md shadow hover:bg-indigo-50 transform duration-300 ease-in-out">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
